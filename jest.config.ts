70070b5eab6e41d68331a2545ac3bcbf19122d0f
juancortes <jcortesj@unal.edu.co>
2024-11-25 22:08:09 -0500

    coverage fix

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,15 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text', 'text-summary'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/*.stories.{js,jsx,ts,tsx}',
+        '!src/**/*.test.{js,jsx,ts,tsx}',
+        '!src/pages/_app.tsx',
+        '!src/pages/_document.tsx',
+    ],
+};
