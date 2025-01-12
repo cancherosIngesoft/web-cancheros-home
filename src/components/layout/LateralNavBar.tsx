6a06d48bca72e4c4c69751d2ec21943318a7dadb
juandp1 <jupalaciosf@unal.edu.co>
2025-01-11 22:38:04 -0500

    change logout redirection and put icon in latereal navbar

--- src/components/layout/LateralNavBar.tsx
+++ src/components/layout/LateralNavBar.tsx
@@ -6,6 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { LogOut } from 'lucide-react'
 import { signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 // Tipo para los elementos de navegación
 type NavItem = {
@@ -26,7 +27,10 @@ export const LateralNavBar = () => {
   };
 
   return (
-    <div className="flex flex-col h-full w-full bg-gradient-to-b from-surface from-60% to-primary-80 to-80% py-4 border-r border-neutral-300">
+    <div className="flex flex-col justify-center align-center h-full w-full bg-gradient-to-b from-surface from-60% to-primary-80 to-80% py-4 border-r border-neutral-300">
+      <Link href="/reservar_cancha" className=" flex justify-center text-2xl font-bold text-[#1A6B51]">
+        <Image src="/LogoWithOutTitle.png" alt="Logo" width={50} height={50} />
+      </Link>
       <div className="flex-grow flex flex-col items-center gap-4">
         {navItems.map((item, index) => (
           <Link key={index} href={item.route}>
@@ -44,7 +48,7 @@ export const LateralNavBar = () => {
             <AvatarFallback className='bg-surface'>{session?.user?.name?.charAt(0)?.toUpperCase() ?? 'U'}</AvatarFallback>
           )}
         </Avatar>
-        <Button variant="ghost" size="icon" onClick={() => signOut()}>
+        <Button variant="ghost" size="icon" onClick={() => signOut({ callbackUrl: '/' })}>
           <LogOut className="h-6 w-6" />
         </Button>
       </div>
