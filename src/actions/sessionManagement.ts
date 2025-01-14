ed7e75dee00ed285c17caff3a9a048766224afeb
juandp1 <jupalaciosf@unal.edu.co>
2025-01-14 13:40:24 -0500

    fix the endpoint connection

--- src/actions/sessionManagement.ts
+++ src/actions/sessionManagement.ts
@@ -1,23 +1,23 @@
 export const userManagement = async (email: string, nombre:string): Promise<any | false> => {
-    // try {
-    //   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/rol_user`, {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ correo: email, nombre: nombre }),
-    //   })
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/rol_user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ correo: email, nombre: nombre }),
+      })
   
-    //   if (res.ok) {
-    //     const data = await res.json()
-    //     return data // Devuelve el JSON completo
-    //   } else {
-    //     return false
-    //   }
-    // } catch (e) {
-    //   console.error("Error en userManagement:", e)
-    //   return false
-    // }
-    return {role: "admin"}
+      if (res.ok) {
+        const data = await res.json()
+        return data // Devuelve el JSON completo
+      } else {
+        return false
+      }
+    } catch (e) {
+      console.error("Error en userManagement:", e)
+      return false
+    }
+   
     
   }
