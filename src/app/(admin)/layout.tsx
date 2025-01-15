de2f5ec9b3a4fa6db5de525f4d959ed10a510cac
juandp1 <jupalaciosf@unal.edu.co>
2025-01-14 19:51:54 -0500

    fixed the lateral navbar

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,20 +1,21 @@
 import { LateralNavBar } from "@/components/layout/LateralNavBar";
 
-export default function admin_layout({
+export default function AdminLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        
-        <div className="w-full h-full flex flex-row">
-            <nav className="w-16 h-full">
+        <div className="flex h-screen w-screen">
+            {/* Barra lateral fija */}
+            <nav className="fixed top-0 left-0 h-full w-16">
                 <LateralNavBar />
-            </nav> 
-            <main className="flex-1 h-full p-12">
-            {children}
-        </main>
-        </div>
+            </nav>
 
+            {/* Contenido principal */}
+            <main className="flex-1 ml-16 h-full overflow-auto p-12 ">
+                {children}
+            </main>
+        </div>
     );
 }
