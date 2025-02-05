99dee836bb1f3097018912a990ad6831654336f1
juancortes <jcortesj@unal.edu.co>
2025-02-05 10:45:16 -0500

    added booking pages and protection of routes

--- src/app/(hostFlow)/panel_negocio/page.tsx
+++ src/app/(hostFlow)/panel_negocio/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+import { useGlobalStore } from "@/store";
+import Link from "next/link";
+
 const pagePanelNegocio = () => {
+  const auth = useGlobalStore((state) => state.auth);
   return (
-    <div>
-      <h1>Panel de Negocio</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Panel de Negocio</h1>
+      <h3>
+        Bienvenido {auth.name}, muy pronto podrás ver aquí las estadísticas de
+        tu negocio. Por el momento, puedes visualizar tus canchas y reservas.
+      </h3>
+      <br></br>
+      <div className="flex flex-col items-center justify-center">
+        <Link href="/mis_canchas" className="hover:text-blue-500">
+          Mis Canchas
+        </Link>
+        <div className="flex flex-col items-center justify-center">
+          <Link href="/reservas_negocio" className="hover:text-blue-500">
+            Visualizar Reservas
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/layout/LateralNavBar.tsx
+++ src/components/layout/LateralNavBar.tsx
@@ -1,44 +1,50 @@
-"use client"
+"use client";
 
-import { useState, useRef, useEffect } from "react"
-import Link from "next/link"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
-import { LogOut } from "lucide-react"
-import { signOut, useSession } from "next-auth/react"
-import Image from "next/image"
-import { useGlobalStore } from "@/store"
-import BallIcon from "../icon/BallIcon"
-import SoccerField from "../icon/SoccerField"
-import TeamShield from "../icon/TeamShield"
-import { PanelClub } from "../club/PanelClub"
+import { useState, useRef, useEffect } from "react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import { useGlobalStore } from "@/store";
+import BallIcon from "../icon/BallIcon";
+import SoccerField from "../icon/SoccerField";
+import TeamShield from "../icon/TeamShield";
+import { PanelClub } from "../club/PanelClub";
 
 type NavItem = {
-  icon: React.ReactNode
-  route: string
-}
+  icon: React.ReactNode;
+  route: string;
+};
 
 const navItems: NavItem[] = [
-  { icon: <BallIcon className="h-10 w-10 text-tertiary" />, route: "/reservar_cancha" },
-  { icon: <SoccerField className="h-8 w-8 text-tertiary stroke-4" />, route: "/mis_reservas" },
-]
+  {
+    icon: <BallIcon className="h-10 w-10 text-tertiary" />,
+    route: "/reservar_cancha",
+  },
+  {
+    icon: <SoccerField className="h-8 w-8 text-tertiary stroke-4" />,
+    route: "/mis_reservas",
+  },
+];
 
 export const LateralNavBar = () => {
-  const [isOpenTeam, setIsOpenTeam] = useState(false)
-  const [navbarWidth, setNavbarWidth] = useState(0)
-  const navbarRef = useRef<HTMLDivElement>(null)
-  const { data: session } = useSession()
-  const authUser = useGlobalStore((state) => state.auth)
+  const [isOpenTeam, setIsOpenTeam] = useState(false);
+  const [navbarWidth, setNavbarWidth] = useState(0);
+  const navbarRef = useRef<HTMLDivElement>(null);
+  const { data: session } = useSession();
+  const authUser = useGlobalStore((state) => state.auth);
 
   useEffect(() => {
     if (navbarRef.current) {
-      setNavbarWidth(navbarRef.current.offsetWidth)
+      setNavbarWidth(navbarRef.current.offsetWidth);
     }
-  }, [])
+  }, []);
 
   const handleLogout = () => {
-    signOut({ callbackUrl: "/" })
-  }
+    signOut({ callbackUrl: "/" });
+  };
 
   return (
     <>
@@ -46,19 +52,41 @@ export const LateralNavBar = () => {
         ref={navbarRef}
         className="flex flex-col justify-center align-center h-full w-full bg-gradient-to-b from-surface from-60% to-primary-80 to-80% py-4 border-r border-neutral-300"
       >
-        <Link href="/" className="flex justify-center text-2xl font-bold text-[#1A6B51]">
-          <Image src="/LogoWithOutTitle.png" alt="Logo" width={50} height={50} />
+        <Link
+          href="/"
+          className="flex justify-center text-2xl font-bold text-[#1A6B51]"
+        >
+          <Image
+            src="/LogoWithOutTitle.png"
+            alt="Logo"
+            width={50}
+            height={50}
+          />
         </Link>
         <section className="flex flex-col items-center gap-4 mt-10">
           {authUser?.userRole === "duenio" && (
-            <Link href="/mis_canchas">
-              <Image src="/icons/miscanchas.svg" alt="Logo" width={50} height={50} />
-            </Link>
+            <>
+              <Link href="/mis_canchas">
+                <Image
+                  src="/icons/miscanchas.svg"
+                  alt="Logo"
+                  width={50}
+                  height={50}
+                />
+              </Link>
+              <Link href="/reservas_negocio">
+                <Image
+                  src="/icons/booking_logo.svg"
+                  alt="Logo"
+                  width={40}
+                  height={40}
+                />
+              </Link>
+            </>
           )}
         </section>
 
         {authUser?.userRole === "jugador" && (
-
           <div className="flex-grow flex flex-col items-center gap-4">
             {navItems.map((item, index) => (
               <Link key={index} href={item.route}>
@@ -72,27 +100,35 @@ export const LateralNavBar = () => {
               <TeamShield className=" text-tertiary stroke-4" />
             </div>
           </div>
-
         )}
-
 
         <div className="flex flex-col items-center gap-4 mt-auto">
           <Avatar>
             {session?.user && session?.user.image ? (
-              <AvatarImage src={session?.user?.image} alt={authUser?.name ?? "User"} />
+              <AvatarImage
+                src={session?.user?.image}
+                alt={authUser?.name ?? "User"}
+              />
             ) : (
               <AvatarFallback className="bg-surface">
                 {session?.user?.name?.charAt(0)?.toUpperCase() ?? "U"}
               </AvatarFallback>
             )}
           </Avatar>
-          <Button variant="ghost" size="icon" onClick={() => signOut({ callbackUrl: "/" })}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => signOut({ callbackUrl: "/" })}
+          >
             <LogOut className="h-6 w-6" />
           </Button>
         </div>
-      </div >
-      <PanelClub isOpen={isOpenTeam} onClose={() => setIsOpenTeam(false)} navbarWidth={navbarWidth} />
+      </div>
+      <PanelClub
+        isOpen={isOpenTeam}
+        onClose={() => setIsOpenTeam(false)}
+        navbarWidth={navbarWidth}
+      />
     </>
-  )
-}
-
+  );
+};
