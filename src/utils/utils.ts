d20f80697211a83e19bba131dd6d5e803a1846b0
juancortes <jcortesj@unal.edu.co>
2025-02-08 22:43:56 -0500

    sugerencias de palacios

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -108,3 +108,22 @@ export function getCanchasId(canchas: {
     };
   });
 }
+
+export async function fetchWithRetry(
+  url: string,
+  options: RequestInit,
+  retries = 3,
+  delay = 1000
+): Promise<Response> {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return response;
+  } catch (error) {
+    if (retries > 0) {
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      return fetchWithRetry(url, options, retries - 1, delay * 2);
+    }
+    throw error;
+  }
+}
