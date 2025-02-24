f74eb85882e6268db66945a2add98e5ad4a44d22
juancortes <jcortesj@unal.edu.co>
2025-02-24 15:01:27 -0500

    Add notifications page and update lateral navbar with notifications link

--- src/components/layout/LateralNavBar.tsx
+++ src/components/layout/LateralNavBar.tsx
@@ -4,7 +4,7 @@ import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { CircleDollarSign, LogOut } from "lucide-react";
+import { Bell, CircleDollarSign, LogOut } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useGlobalStore } from "@/store";
@@ -25,13 +25,15 @@ const navItems: NavItem[] = [
     route: "/reservar_cancha",
   },
   {
-    icon: <Image
-    className="md:w-10 md:h-10 w-10 h-10"
-    src="/icons/miscanchas.svg"
-    alt="Logo"
-    width={40}
-    height={40}
-  />,
+    icon: (
+      <Image
+        className="md:w-10 md:h-10 w-10 h-10"
+        src="/icons/miscanchas.svg"
+        alt="Logo"
+        width={40}
+        height={40}
+      />
+    ),
     route: "/mis_reservas",
   },
 ];
@@ -61,19 +63,18 @@ export const LateralNavBar = () => {
         ref={navbarRef}
         className="flex  z-10 flex-row md:flex-col justify-center md:justify-between md:align-center h-full w-full bg-primary-95 md:bg-gradient-to-b from-surface from-60% to-primary-80 to-80% md:py-4  border-t md:border-r border-neutral-300"
       >
-        
         <section className="flex flex-row md:flex-col items-center justify-center  gap-8  md:gap-4 ">
-        <Link
-          href="/"
-          className="md:flex justify-center font-bold text-[#1A6B51] hidden md:mt-4"
-        >
-          <Image
-            src="/LogoWithOutTitle.png"
-            alt="Logo"
-            width={50}
-            height={50}
-          />
-        </Link>
+          <Link
+            href="/"
+            className="md:flex justify-center font-bold text-[#1A6B51] hidden md:mt-4"
+          >
+            <Image
+              src="/LogoWithOutTitle.png"
+              alt="Logo"
+              width={50}
+              height={50}
+            />
+          </Link>
           {authUser?.userRole === "duenio" && (
             <>
               <Link href="/mis_canchas">
@@ -117,7 +118,7 @@ export const LateralNavBar = () => {
 
           {authUser?.userRole === "jugador" && (
             <div className="flex flex-row md:flex-col justify-center items-center w-full h-full gap-8">
-            {navItems.map((item, index) => (
+              {navItems.map((item, index) => (
                 <Link key={index} href={item.route} className="h-fit w-fit">
                   {item.icon}
                 </Link>
@@ -127,14 +128,16 @@ export const LateralNavBar = () => {
                 onClick={() => setIsOpenTeam(!isOpenTeam)}
               >
                 <TeamShield className="md:w-10 md:h-10 w-10 h-10 text-tertiary stroke-4" />
+                <Link
+                  href="/notificaciones"
+                  className="flex items-center justify-center  hover:text-green-600 transition-colors"
+                >
+                  <Bell className="md:w-10 md:h-10 w-8 h-8 text-green-800" />
+                </Link>
               </div>
             </div>
-              
-            
           )}
         </section>
-
-
 
         <div className="flex  flex-row md:flex-col items-center ml-4 md:ml-0 gap-4 ">
           <Avatar className="hidden md:block">
@@ -149,22 +152,17 @@ export const LateralNavBar = () => {
               </AvatarFallback>
             )}
           </Avatar>
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={handleLogout}
-          >
+          <Button variant="ghost" size="icon" onClick={handleLogout}>
             <LogOut className="h-6 w-6" />
           </Button>
         </div>
         <PanelClub
-        isOpen={isOpenTeam}
-        onClose={() => setIsOpenTeam(false)}
-        navbarWidth={navbarWidth}
-        navbarHeight={navbarHeight}
-      />
+          isOpen={isOpenTeam}
+          onClose={() => setIsOpenTeam(false)}
+          navbarWidth={navbarWidth}
+          navbarHeight={navbarHeight}
+        />
       </div>
-      
     </>
   );
 };
