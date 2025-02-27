cc5a321ac39a14fbe404e595df4d75e7cbd4c51c
juancortes <jcortesj@unal.edu.co>
2025-02-27 00:17:59 -0500

    fokin cancheros

--- src/app/(hostFlow)/mis_canchas/page.tsx
+++ src/app/(hostFlow)/mis_canchas/page.tsx
@@ -77,7 +77,7 @@ export default function pageMisCanchas() {
           ) : (
             <>
               <Image
-                src="/icons/canchaGris.svg"
+                src="/icons/soccerField.svg"
                 alt="Logo"
                 width={150}
                 height={150}
